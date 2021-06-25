d34a2332bf6b04be15e14721d3cdd2a59e5665aa
Илья Гавшин <gafhin03@gmail.com>
2021-06-25 11:10:53 +0300

    Создан основной класс в иерархии вещей в каталоге, и отедельны класс для книг

---
+++ CatalogCore/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogCore
+{
+    class Book: MainElement
+    {
+        protected string authors;
+
+        protected string illustrator { private set; get; } = "отстутствует";
+
+        protected string publishingHouse{ private set; get;}
+
+        protected int yearOfPublication { private set; get; }
+
+        public Book(string na, string own, string aut, string ill, string pubHouse, int yePubl) : base(na,own)
+        {
+            authors = aut;
+            illustrator = ill;
+            publishingHouse = pubHouse;
+            yearOfPublication = yePubl;
+        }
+        public Book(string na, string own, string aut, string pubHouse, int yePubl) : base(na,own)   
+        {
+            authors = aut;
+            publishingHouse = pubHouse;
+            yearOfPublication = yePubl;
+        }
+
+        public virtual void Change(string name, string aut, string ill, string pubHouse, int yePubl)
+        {
+            Change(name);
+            authors = aut;
+            if(ill != null) { illustrator = ill; }
+            publishingHouse = pubHouse;
+            yearOfPublication = yePubl;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Name: {Name}" +
+                $"\n    Owner Name: {ownerName}, Data transfer: {transferDate}" +
+                $"\n    Authors: {authors},  Illustrator: {illustrator}, Publishing House: {publishingHouse}, Year Publication: {yearOfPublication}";
+        }
+    }
+}
---
+++ CatalogCore/MainElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogCore
+{
+    class MainElement
+    {
+        public string Name { private set;  get; }
+
+        public string ownerName { private set; get; }
+
+        public DateTime transferDate { private set; get; }
+
+        public MainElement(string name, string owName)
+        {
+            Name = name;
+            ownerName = owName;
+            transferDate = DateTime.Now;
+        }
+
+        public virtual void Change(string name)
+        { 
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Name: {Name}, Owner Name: {ownerName}, Data transfer: {transferDate}";
+        }
+
+    }
+}
